dff573bcdaff41dd67acbb3813cd22a8e03ed823
Ernest Arutiunian <arutiunian.ernest@gmail.com>
2021-05-28 21:36:27 -0400

    Functionality is set  TODO: minor edits and read me

--- script/script.js
+++ script/script.js
@@ -1,63 +1,38 @@
-var timeBlock = $(".time-block")
-
-var storedData = {
-    9:null,
-    10:null,
-    11:null,
-    12:null,
-    1:null,
-    2:null,
-    3:null,
-    4:null,
-    5:null,
-};
-
-// function init() {
-
-// }
-
-
+var timeBlock = $(".time-block");
 
 function getTime() {
-    var hour = moment().format("H")
-     console.log(hour);
-    timeBlock.each(function( index ) {
-         var block = $(this).children().eq(1)
-        var currentId = $(this).children().eq(1).attr("id")
-         console.log(currentId);
-        console.log(block);
-         console.log(timeBlock);
-         if(currentId===hour) {
-            block.addClass("present");
-         } else if (currentId < hour) {
-            block.addClass("past");
-         } else if (currentId > hour) {
-            block.addClass("future");
-         }
+  var hour = moment().format("HH");
+  timeBlock.each(function (index) {
+    var block = $(this).children().eq(1);
+    var currentId = $(this).children().eq(1).attr("id");
+    if (currentId === hour) {
+      block.addClass("present");
+    } else if (currentId < hour) {
+      block.addClass("past");
+    } else if (currentId > hour) {
+      block.addClass("future");
     }
-    )}
-
-getTime()
-
-// showData()
-
-
-
-function storeData() {
-    console.log(storedData);
-    localStorage.setItem("hours", JSON.stringify(storedData))
+  });
 }
 
+function showData() {
+  timeBlock.each(function (index) {
+    var block = $(this).children().eq(1);
+    var currentId = $(this).children().eq(1).attr("id");
+    var blocktask = localStorage.getItem(currentId);
+    console.log(blocktask);
+    if (blocktask !== null) {
+      var attachTask = JSON.parse(blocktask);
+      block.text(attachTask);
+    }
+  });
+}
 
+timeBlock.on("click", "#save", function (event) {
+  var textItem = $(this).parent().children().eq(1);
+  var saveData = textItem.val();
+  var hour = textItem.attr("id");
+  localStorage.setItem(hour, JSON.stringify(saveData));
+});
 
-// $(document).ready(init())
-
-
-timeBlock.on("click", ("#save"), function(event) {
-    var textItem = $(this).parent().children().eq(1);
-    var saveData = textItem.val();
-    var hour = textItem.attr("id");
-    storedData[hour] = saveData;
-    storeData();
-})
-
+$(document).ready(showData(), getTime());
